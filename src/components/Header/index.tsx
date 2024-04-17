7b57dc44c17b84fea2d32d10851cbea4353bf79a
nilberm <nilber16@outlookcom>
2024-04-17 15:55:43 -0300

    feat(header): component started

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import style from "./style.module.scss";
+
+import { FaSistrix } from "react-icons/fa6";
+
+import logo from "@/assets/logo/logo.png";
+import Link from "next/link";
+
+interface HeaderProps {}
+export default function Header(props: HeaderProps) {
+  return (
+    <header className={style.header}>
+      <div className={style.logo}>
+        <Image src={logo} alt="Luxo de Bíblia" />
+      </div>
+      <div className={style.search}>
+        <input type="text" placeholder="Pesquisar" />
+        <span className={style.icon}>
+          <FaSistrix />
+        </span>
+      </div>
+      <div className={style.links}>
+        <div className={style.link}>
+          <Link href="#" target="_blank" rel="noopener noreferrer">
+            Minha Conta
+          </Link>
+        </div>
+        <div className={style.link}>
+          <Link href="#" target="_blank" rel="noopener noreferrer">
+            <span className={style.summary}>R$0,00</span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
